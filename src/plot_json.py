17d10276980b382bf0a319233060df8e10f8a23e
Cheng <ccheng2@andrew.cmu.edu>
2021-03-12 18:28:02 -0500

    fixed plotting erros

--- src/plot_json.py
+++ src/plot_json.py
@@ -4,12 +4,35 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 from dash.dependencies import Output, Input
+import plotly.graph_objs as go
+
 
 cache = "fig.json"
 # Construct a figure object and save it as json.
-df = px.data.iris()
-fig = px.scatter(df, x="sepal_width", y="sepal_length")
+# df = px.data.iris()
+# fig = px.scatter(df, x="sepal_width", y="sepal_length")
+
+fig= go.Figure()
+fig.add_trace(go.Scatter(
+    x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 10],
+    y=[0, 4, 5, 1, 2, 3, 2, 4, 2, 1],
+    mode="text",
+    text=["","","","", "No data.", "","","", "", ''],
+    textfont_size=40,
+    ))
+
+fig.update_layout(
+    font = dict(size=16),
+    margin={'l':0, 'r':0, 't':0, 'b':0},
+    width = 480,
+    height=300
+)
+
+#fig= go.Figure()
+
+
 with open(cache, 'w') as f:
+
     f.write(fig.to_json())
 # Create example app.
 app = dash.Dash(prevent_initial_callbacks=True)
@@ -18,8 +41,8 @@ app.layout = html.Div([dcc.Graph(id="graph"), html.Button("Click me", id="btn")]
 
 @app.callback(Output("graph", "figure"), [Input("btn", "n_clicks")])
 def func(n_clicks):
-    with open(cache, 'r') as f:
-        return json.load(f)
+    #with open(cache, 'r') as f:
+    return fig
 
 if __name__ == '__main__':
     app.run_server()
--- src/plotting_utils.py
+++ src/plotting_utils.py
@@ -13,8 +13,10 @@ from utils import *
 import plotly.express as px
 pd.options.plotting.backend = "plotly"
 import pickle
-IMGDIR = "cache_figs"
+import plotly.graph_objs as go
 
+IMGDIR = "cache_figs"
+REWRITE = False
 def get_datafiles_ready():
     min_date = '2020-03-26'
     max_date = "2021-02-01"
@@ -27,9 +29,10 @@ def get_datafiles_ready():
     cdc['race_ethnicity_combined'].cat.rename_categories({'Asian, Non-Hispanic':'Asian',
                                  'Black, Non-Hispanic':'Black',
                                   'White, Non-Hispanic':'White',
-                                   'Multiple/Other, Non-Hispanic ':'Multiple/Other',
-                                    'Native Hawaiian/Other Pacific Islander, Non-Hispanic':'Native Hawaiian/Other Pacific Islander',
-                                    'American Indian/Alaska Native, Non-Hispanic':'American Indian/Alaska Native'},
+                                   'Multiple/Other, Non-Hispanic ':'Multiple or Other',
+                                    'Native Hawaiian/Other Pacific Islander, Non-Hispanic':'Native Hawaiian or Other Pacific Islander',
+                                    'American Indian/Alaska Native, Non-Hispanic':'American Indian or Alaska Native',
+                                    'Hispanic/Latino': 'Hispanic or Latino'},
                                     inplace=True)
     print("Finished loading CDC data.")
     florida = florida[(florida['Case_']>=min_date) & (florida['Case_']<=max_date)]
@@ -40,7 +43,7 @@ def florida_case_hosp_death(florida, gender='All', time='Case_', race='All', sta
     fname = "time_{}_gender_{}_race_{}_state_{}_".format(time, gender, race, state)
     figs = []
     vrs = ['all', 'Hospitalized', 'Died'] # 'Died'
-    if not os.path.exists(os.path.join(IMGDIR,fname + vrs[0] +'.json')):
+    if not os.path.exists(os.path.join(IMGDIR,fname + vrs[0] +'.json')) or REWRITE:
         florida['all'] = 1
         lbs = ['COVID-19 Cases','COVID-19 Hospitalizations', 'COVID-19 Deaths']
         vnames = ['Cases', 'Hospitalizations', 'Deaths']
@@ -62,39 +65,59 @@ def florida_case_hosp_death(florida, gender='All', time='Case_', race='All', sta
             if state != 'All':
                 newdf = newdf[newdf['res_state']==state]
         
+     
 
         for i, v in enumerate(vrs):
+            
+            #return None
             amt=v
-            df = get_groupby_rolling_df(newdf, grpvar, freq, amt, time)
-            ylb = 'Count' #if i==0 else ''
-            xlb = 'Positive Confirmed Date' if time=='Case_' else 'CDC Report Date'
-            df = df[df[grpvar]!='_Unknown']
-            df[grpvar].cat.remove_unused_categories(inplace=True)
+            if not(newdf is None or len(newdf)==0):
+                df = get_groupby_rolling_df(newdf, grpvar, freq, amt, time)
+                ylb = 'Count' #if i==0 else ''
+                xlb = 'Positive Confirmed Date' if time=='Case_' else 'CDC Report Date'
+                df = df[df[grpvar]!='_Unknown']
+                df[grpvar].cat.remove_unused_categories(inplace=True)
 
-            df.groupby(grpvar)['rolling sum'].plot()#(ylabel=ylb, xlabel=xlb, ax=ax[i])
-            df = df.reset_index()
-            df = df.rename(columns={time:"Date", 'rolling sum':vnames[i], 'Age_group':'Age Group'})
-            df.loc[df[vnames[i]] <=5, vnames[i]] = np.nan
+                df.groupby(grpvar)['rolling sum'].plot()#(ylabel=ylb, xlabel=xlb, ax=ax[i])
+                df = df.reset_index()
+                df = df.rename(columns={time:"Date", 'rolling sum':vnames[i], 'Age_group':'Age Group'})
+                df.loc[df[vnames[i]] <=5, vnames[i]] = np.nan
 
-            fig = px.line(df, x='Date', y=vnames[i], color='Age Group')
+                fig = px.line(df, x='Date', y=vnames[i], color='Age Group')
 
-            showlegend = True #if i==2 else False
-            fig.update_xaxes(
-                dtick = 'M1',
-                tickformat = "%b\n  %Y",
-                tickangle = 30
-            )
-            fig.update_layout(xaxis_title = xlb,
-                            yaxis_title = ylb,
-                            legend_title = "Age Group", 
-                            yaxis={'categoryorder': 'total ascending'},
-                            legend={'traceorder': 'reversed', 'font':{'size':10}},
-                            font = dict(size=16),
-                            showlegend = showlegend,
-                            margin={'l':0, 'r':0, 't':0, 'b':0},
-                            width = 480,
-                            height=300
-                            )
+                showlegend = True #if i==2 else False
+                fig.update_xaxes(
+                    dtick = 'M1',
+                    tickformat = "%b\n  %Y",
+                    tickangle = 30
+                )
+                fig.update_layout(xaxis_title = xlb,
+                                yaxis_title = ylb,
+                                legend_title = "Age Group", 
+                                yaxis={'categoryorder': 'total ascending'},
+                                legend={'traceorder': 'reversed', 'font':{'size':10}},
+                                font = dict(size=16),
+                                showlegend = showlegend,
+                                margin={'l':0, 'r':0, 't':0, 'b':0},
+                                width = 480,
+                                height=300)
+            else:
+                fig= go.Figure()
+                fig.add_trace(go.Scatter(
+                    x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 10],
+                    y=[0, 4, 5, 1, 2, 3, 2, 4, 2, 1],
+                    mode="text",
+                    text=["","","","", "No data.", "","","", "", ''],
+                    textfont_size=40,
+                    ))
+
+                fig.update_layout(
+                    font = dict(size=16),
+                    margin={'l':0, 'r':0, 't':0, 'b':0},
+                    width = 480,
+                    height=300
+                )
+        
             fn = os.path.join(IMGDIR,fname+vrs[i]+'.json')
             print("caching:", fn)
             with open(fn, 'w') as f:
@@ -107,7 +130,7 @@ def florida_case_hosp_death(florida, gender='All', time='Case_', race='All', sta
             print("loading:", fn)
             with open(fn, 'r') as f:
                 fig = json.load(f)
-                figs.append(fig)
+            figs.append(fig)
 
     return figs
 
@@ -116,7 +139,7 @@ def florida_case_hosp_death_agg(florida, gender='All', time='Case_', race='All',
     fname = "agg_time_{}_gender_{}_race_{}_state_{}_".format(time, gender, race, state)
     figs = []
     vrs = ['all', 'Hospitalized', 'Died'] # 'Died'
-    if not os.path.exists(os.path.join(IMGDIR,fname + vrs[0] +'.json')):
+    if not os.path.exists(os.path.join(IMGDIR,fname + vrs[0] +'.json')) or REWRITE:
         florida['all'] = 1
         lbs = ['COVID-19 Cases','COVID-19 Hospitalizations', 'COVID-19 Deaths']
         vnames = ['Cases', 'Hospitalizations', 'Deaths']
@@ -138,43 +161,66 @@ def florida_case_hosp_death_agg(florida, gender='All', time='Case_', race='All',
             if state != 'All':
                 newdf = newdf[newdf['res_state']==state]
         
+        # marginal case when it is empty
+        if newdf is None or len(newdf)==0:
+            return None
 
         figs = []
         for i, v in enumerate(vrs):
-            amt=v
-            df = get_groupby_rolling_df(newdf, grpvar, freq, amt, time)
-            ylb = 'Count' #if i==0 else ''
-            xlb = 'Positive Confirmed Date' if time=='Case_' else 'CDC Report Date'
-            #df = df[df[grpvar]!='_Unknown']
-            #df[grpvar].cat.remove_unused_categories(inplace=True)
+            if not(newdf is None or len(newdf)==0):
+                amt=v
+                df = get_groupby_rolling_df(newdf, grpvar, freq, amt, time)
+                ylb = 'Count' #if i==0 else ''
+                xlb = 'Positive Confirmed Date' if time=='Case_' else 'CDC Report Date'
+                #df = df[df[grpvar]!='_Unknown']
+                #df[grpvar].cat.remove_unused_categories(inplace=True)
 
-            #df.groupby(grpvar)['rolling sum'].plot()#(ylabel=ylb, xlabel=xlb, ax=ax[i])
-            df = get_rolling_df(newdf, time, freq=7, var=amt)
-            df = df.reset_index()
-            df = df.rename(columns={time:"Date", amt:vnames[i]})
-            df.loc[df[vnames[i]] <=5, vnames[i]] = np.nan
+                #df.groupby(grpvar)['rolling sum'].plot()#(ylabel=ylb, xlabel=xlb, ax=ax[i])
+                df = get_rolling_df(newdf, time, freq=7, var=amt)
+                df = df.reset_index()
+                df = df.rename(columns={time:"Date", amt:vnames[i]})
+                df.loc[df[vnames[i]] <=5, vnames[i]] = np.nan
 
-            fig = px.line(df, x='Date', y=vnames[i])
+                fig = px.line(df, x='Date', y=vnames[i])
 
-            showlegend = True #if i==2 else False
-            fig.update_xaxes(
-                dtick = 'M1',
-                tickformat = "%b\n  %Y",
-                tickangle = 30
-            )
-            fig.update_layout(xaxis_title = xlb,
-                            yaxis_title = ylb,
-                            legend_title = "Age Group", 
-                            yaxis={'categoryorder': 'total ascending'},
-                            legend={'traceorder': 'reversed', 'font':{'size':10}},
-                            font = dict(size=16),
-                            showlegend = showlegend,
-                            margin={'l':0, 'r':0, 't':0, 'b':0},
-                            width = 480,
-                            height=300
-                            )
+                showlegend = True #if i==2 else False
+                fig.update_xaxes(
+                    dtick = 'M1',
+                    tickformat = "%b\n  %Y",
+                    tickangle = 30
+                )
+                fig.update_layout(xaxis_title = xlb,
+                                yaxis_title = ylb,
+                                legend_title = "Age Group", 
+                                yaxis={'categoryorder': 'total ascending'},
+                                legend={'traceorder': 'reversed', 'font':{'size':10}},
+                                font = dict(size=16),
+                                showlegend = showlegend,
+                                margin={'l':0, 'r':0, 't':0, 'b':0},
+                                width = 480,
+                                height=300)
+                                
+
+            else:
+                fig= go.Figure()
+                fig.add_trace(go.Scatter(
+                    x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 10],
+                    y=[0, 4, 5, 1, 2, 3, 2, 4, 2, 1],
+                    mode="text",
+                    text=["","","","", "No data.", "","","", "", ''],
+                    textfont_size=40,
+                    ))
+
+                fig.update_layout(
+                    font = dict(size=16),
+                    margin={'l':0, 'r':0, 't':0, 'b':0},
+                    width = 480,
+                    height=300
+                )
+
             fn = os.path.join(IMGDIR,fname+vrs[i]+'.json')
             print("caching:", fn)
+            #pdb.set_trace()
             with open(fn, 'w') as f:
             #     pickle.dump(html_bytes, f)
                 f.write(fig.to_json())
@@ -185,7 +231,7 @@ def florida_case_hosp_death_agg(florida, gender='All', time='Case_', race='All',
             print("loading:", fn)
             with open(fn, 'r') as f:
                 fig = json.load(f)
-                figs.append(fig)
+            figs.append(fig)
     return figs
 
 def age_distribution_plots(florida, gender='All', time='Case_', race='All', state='All'):
@@ -193,7 +239,7 @@ def age_distribution_plots(florida, gender='All', time='Case_', race='All', stat
     figs = []
     vrs = ['all', 'Hospitalized', 'Died'] # 'Died'
 
-    if not os.path.exists(os.path.join(IMGDIR,fname + vrs[0] +'.json')):
+    if not os.path.exists(os.path.join(IMGDIR,fname + vrs[0] +'.json')) or REWRITE:
         florida['all'] = 1
 
         lbs = ['COVID-19 Cases','Hospitalizations', 'Deaths']
@@ -213,46 +259,64 @@ def age_distribution_plots(florida, gender='All', time='Case_', race='All', stat
 
         
         for i, v in enumerate(vrs):
-            amt = v
-            newdf = florida.copy()
-            min_date = "2020-04-01"
-            max_date = "2021-02-01"
-            ylb = 'Ratio' 
-            xlb = 'Positive Confirmed Date' if time=='Case_' else 'CDC Report Date'
-            if grpvar == 'Age_group':
-                newdf = remove_unkown(newdf, grpvar)
-            freq = '7D'
-            df = get_groupby_rolling_df(newdf, grpvar, freq, amt, time)
-            df[amt] = df['rolling sum']
-            daily = get_rolling_df(newdf, time=time, freq=7, var=amt)
-            daily[amt].fillna(0, inplace=True)
-            daily.rename(columns={amt:'died_daily'}, inplace=True)
+            if not(newdf is None or len(newdf)==0):
+                amt = v
+                newdf = florida.copy()
+                min_date = "2020-04-01"
+                max_date = "2021-02-01"
+                ylb = 'Ratio' 
+                xlb = 'Positive Confirmed Date' if time=='Case_' else 'CDC Report Date'
+                if grpvar == 'Age_group':
+                    newdf = remove_unkown(newdf, grpvar)
+                freq = '7D'
+                df = get_groupby_rolling_df(newdf, grpvar, freq, amt, time)
+                df[amt] = df['rolling sum']
+                daily = get_rolling_df(newdf, time=time, freq=7, var=amt)
+                daily[amt].fillna(0, inplace=True)
+                daily.rename(columns={amt:'died_daily'}, inplace=True)
 
-            merged = df.merge(daily, left_index=True, right_index=True)
-            merged['died_ratio'] = merged[amt] / merged['died_daily']
-            
-            m = merged[(merged.index >=min_date) & (merged.index<=max_date)]
-            m = m.reset_index()
-            print(m.columns)
-            m = m.rename(columns={time:"Date", 'died_ratio':vnames[i], 'Age_group':'Age Group'})
+                merged = df.merge(daily, left_index=True, right_index=True)
+                merged['died_ratio'] = merged[amt] / merged['died_daily']
+                
+                m = merged[(merged.index >=min_date) & (merged.index<=max_date)]
+                m = m.reset_index()
+                print(m.columns)
+                m = m.rename(columns={time:"Date", 'died_ratio':vnames[i], 'Age_group':'Age Group'})
 
-            fig = px.bar(m, x='Date', y=vnames[i], color="Age Group", barmode = 'stack')        
-            
-            fig.update_xaxes(
-                    dtick = 'M1',
-                    tickformat = "%b\n  %Y",
-                    tickangle = 30
-            )
-            fig.update_layout(xaxis_title = xlb,
-                            yaxis_title = ylb,
-                            legend_title = "Age Group", 
-                            legend_title_font_size = 12,
-                            yaxis={'categoryorder': 'total ascending'},
-                            legend={'traceorder': 'reversed', 'font':{'size':10}},
-                            font = dict(size=16),
-                            width = 480,
-                            height=350
-                            )    
+                fig = px.bar(m, x='Date', y=vnames[i], color="Age Group", barmode = 'stack')        
+                
+                fig.update_xaxes(
+                        dtick = 'M1',
+                        tickformat = "%b\n  %Y",
+                        tickangle = 30
+                )
+                fig.update_layout(xaxis_title = xlb,
+                                yaxis_title = ylb,
+                                legend_title = "Age Group", 
+                                legend_title_font_size = 12,
+                                yaxis={'categoryorder': 'total ascending'},
+                                legend={'traceorder': 'reversed', 'font':{'size':10}},
+                                font = dict(size=16),
+                                width = 480,
+                                height=350
+                                )    
+            else:
+                fig= go.Figure()
+                fig.add_trace(go.Scatter(
+                    x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 10],
+                    y=[0, 4, 5, 1, 2, 3, 2, 4, 2, 1],
+                    mode="text",
+                    text=["","","","", "No data.", "","","", "", ''],
+                    textfont_size=40,
+                    ))
+
+                fig.update_layout(
+                    font = dict(size=16),
+                    margin={'l':0, 'r':0, 't':0, 'b':0},
+                    width = 480,
+                    height=300
+                )
+    
             fn = os.path.join(IMGDIR,fname+vrs[i]+'.json')
             print("caching:", fn)
             with open(fn, 'w') as f:
@@ -265,11 +329,10 @@ def age_distribution_plots(florida, gender='All', time='Case_', race='All', stat
             print("loading:", fn)
             with open(fn, 'r') as f:
                 fig = json.load(f)
-                figs.append(fig)
+            figs.append(fig)
 
     return figs
 
-# 
 # %%
 if __name__ == '__main__':
     florida, cdc = get_datafiles_ready()
@@ -279,14 +342,43 @@ if __name__ == '__main__':
     states_cats = cdc['res_state'].dropna().unique()#.tolist()
     states_cats = ['All']+[x for x in states_cats if (x != 'Missing') and (x!= 'Unknown')]
 
+    total = 3*3*3 + 3*len(race_cats)*len(states_cats)*9
+    count = 0
     for gender in ['All', 'Female', 'Male']:
         figs = florida_case_hosp_death_agg(florida, gender)
         figs = florida_case_hosp_death(florida, gender)
         figs = age_distribution_plots(florida, gender)
-
+        count += 9
 
         for race in race_cats:
             for state in states_cats:
                 figs = florida_case_hosp_death_agg(cdc, gender, 'cdc_case_earliest_dt', race, state)
                 figs = florida_case_hosp_death(cdc, gender, 'cdc_case_earliest_dt', race, state)
                 figs = age_distribution_plots(cdc, gender, 'cdc_case_earliest_dt', race, state)
+                count += 9
+                print("******Count:{}/{}".format(count, total))
+
+
+# %%
+# if __name__ == '__main__':
+#     florida, cdc = get_datafiles_ready()
+#     gender = 'All'
+#     race = 'Black'
+#     state = 'NV'
+#     # DIED
+#     figs = florida_case_hosp_death_agg(cdc, gender, 'cdc_case_earliest_dt', race, state)
+
+
+# %%
+# f = "cache_figs/agg_time_cdc_case_earliest_dt_gender_All_race_Hispanic_Latino_state_MO_all.json"
+
+# with open(f, 'w') as f:
+#     print('opened')
+
+# %%
+
+# %%
+
+# %%
+print('why')
+# %%
